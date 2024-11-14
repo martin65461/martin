4aeb6bd04d09f65cc99d4a2990a6a4f029145f84
martin65461 <mrtin64540d@gmail.com>
2024-11-14 17:58:26 -0300

    Add files via upload

---
+++ java/java.js
@@ -0,0 +1,25 @@
+function inclusive ()
+  {
+  document.body.style.backgroundColor = "white";
+document.getElementById("x").style.color = "black";
+document.getElementById("pagdrece").style.color = "white";
+document.getElementById("bacck").style.backgroundColor = "#A9A9A9";
+document.getElementById("navegaa").style.backgroundColor = "#A9A9A9";
+document.getElementById("bacck").style.backgroundColor = "#A9A9A9";
+document.getElementById("proo").style.backgroundColor = "#A9A9A9";
+document.getElementById("footeer").style.backgroundColor = "#A9A9A9";
+document.getElementById("containeer").style.backgroundColor = "#A9A9A9";
+document.getElementById("headeer").style.backgroundColor = "#A9A9A9";
+
+document.getElementById("pagdguaz").style.color = "white";
+document.getElementById("pagdchip").style.color = "white";
+document.getElementById("pagdemju").style.color = "white";
+document.getElementById("pagdecontac").style.color = "white";
+
+document.getElementById("").style.color = "white";
+document.getElementById("").style.color = "white";
+document.getElementById("").style.color = "white";
+document.getElementById("").style.color = "white";
+
+  }
+function creator(){alert("sitio creado por Martin Alejandro Duarte")}
